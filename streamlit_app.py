2e15956bb5b042dd3674bf17f2141c5afb99c602
Gerardo Bohorquez <88528474+gerardo5797@users.noreply.github.com>
2024-06-07 11:56:04 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,5 +48,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered,' + name_on_order + '!', icon='✅')
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    st.text(fruityvice_response)
